d5526d8ecb30dcb14173ecd87f8956f8a70057e0
aleksanderpiotrowski <aleksander.piotrowski2@orange.com>
2020-12-05 13:38:40 +0100

    init

---
+++ src/main/java/pl/orange/reactorworkshop/ReactorworkshopApplication.java
@@ -0,0 +1,13 @@
+package pl.orange.reactorworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorworkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorworkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/orange/reactorworkshop/ReactorworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.orange.reactorworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactorworkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
